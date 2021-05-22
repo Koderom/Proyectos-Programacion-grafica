a84c2be80116ea785c1b11cb3b1a4a4d2b3b75e0
OchoaMirko <69165716+Koderom@users.noreply.github.com>
2021-05-22 17:02:22 -0400

    Add files via upload

---
+++ OpenGLGameCS/OpenGLGameCS/Program.cs
@@ -0,0 +1,22 @@
+﻿using OpenGLGameCS.Game;
+using OpenTK.Windowing.Desktop;
+using System;
+
+namespace OpenGLGameCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (GameEngine game = new GameEngine(GameWindowSettings.Default, NativeWindowSettings.Default))
+            {
+                game.Initialise();
+                game.RunGameLoop();
+            }
+
+            Console.WriteLine("Game has come to an end");
+        }
+    }
+}
